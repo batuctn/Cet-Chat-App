17251a08e9dcd7c064081300b2165a37b069ae58
batuctn <batuctnn14@gmail.com>
2022-10-09 23:51:06 +0300

    firebase config add + firebase functions add

---
+++ functions/index.js
@@ -0,0 +1,22 @@
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+const { firestore } = require("firebase-admin");
+admin.initializeApp();
+
+exports.removeExpiredDocuments = functions.pubsub
+  .schedule("every 1 hours")
+  .onRun(async (context) => {
+    const db = admin.firestore();
+    const now = firestore.Timestamp.now();
+    const ts = firestore.Timestamp.fromMillis(now.toMillis() - 86400000); // 24 hours in milliseconds = 86400000
+
+    const snap = await db
+      .collection("users")
+      .where("storyPhotoUrl", "<", ts)
+      .get();
+    let promises = [];
+    snap.forEach((snap) => {
+      promises.push(snap.ref.delete());
+    });
+    return Promise.all(promises);
+  });
